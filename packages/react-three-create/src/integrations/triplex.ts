4b92d4f949e0b62745e4c9ea90dfd120b6b584d0
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-05-12 10:28:21 +1000

    chore: update

--- packages/react-three-create/src/integrations/triplex.ts
+++ packages/react-three-create/src/integrations/triplex.ts
@@ -8,4 +8,8 @@ export function generateTriplex(generator: Generator, options: GenerateTriplexOp
   }
 
   generator.inject('vscode-extension-suggestion', 'trytriplex.triplex-vsce')
+  generator.inject(
+    'readme-tools',
+    '[Triplex](https://triplex.dev) - Build the 2D and 3D web without coding. Your visual workspace for React / Three Fiber.',
+  )
 }
